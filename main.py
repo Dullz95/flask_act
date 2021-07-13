54d9974c6932695b07ebdf135ed651df46eb2a17
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-07-13 13:05:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, url_for
+
+app=Flask(__name__)
+
+@app.route('/validate/<name>')
+def check_user(name):
+    if name == 'Abdullah':
+        return redirect(url_for('admin_page', name=name))
+    elif name == 'Godwin':
+        return redirect(url_for('user_page',name=name))
+    else:
+        if name == 'Guest':
+            return redirect(url_for('guest_pge',name=name))
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'hello user %s ' % name
+
+@app.route('/admin/<name>')
+def admin_page(name):
+    return 'Welcome to the admin page %s ' % name
+
+@app.route('/Guest/<name>')
+def guest_page(name):
+    return 'Welcome to the guest page'
+
+@app.route('/currency/<float:salary>')
+def salary_check(salary):
+    if salary < 1050.500:
+        return redirect('https://www.sahomeloans.com')
+    else:
+       if salary > 1050.500:
+           return redirect('https://www.fnb.co.za')
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
